be3af5abc0a3ae97f159033e78c79e1d2b13808e
XeindsTwo <you@example.com>
2026-04-10 12:20:59 +0300

    new

--- pizzas/urls.py
+++ pizzas/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 
-from pizzas.views import PizzaList
+from pizzas.views import PizzaList, PizzaCreate, PizzaDetail, PizzaUpdate, PizzaDelete
 
 urlpatterns = [
     path('pizzas/', PizzaList.as_view(), name='pizza-list'),
+    path('pizzas/', PizzaCreate.as_view(), name='pizza-create'),
+    path('pizzas/<int:pk>', PizzaDetail.as_view(), name='pizza-detail'),
+    path('pizzas/<int:pk>', PizzaUpdate.as_view(), name='pizza-update'),
+    path('pizzas/<int:pk>', PizzaDelete.as_view(), name='pizza-delete'),
 ]
--- pizzas/views.py
+++ pizzas/views.py
@@ -1,7 +1,28 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
 from .models import Pizza
 from .serializers import PizzaSerializer
 
+
 class PizzaList(ListAPIView):
     queryset = Pizza.objects.order_by('-price')
+    serializer_class = PizzaSerializer
+
+
+class PizzaCreate(CreateAPIView):
+    queryset = Pizza.objects.all()
+    serializer_class = PizzaSerializer
+
+
+class PizzaDetail(RetrieveAPIView):
+    queryset = Pizza.objects.all()
+    serializer_class = PizzaSerializer
+
+
+class PizzaUpdate(UpdateAPIView):
+    queryset = Pizza.objects.all()
+    serializer_class = PizzaSerializer
+
+
+class PizzaDelete(DestroyAPIView):
+    queryset = Pizza.objects.all()
     serializer_class = PizzaSerializer
